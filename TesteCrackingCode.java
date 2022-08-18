b9fd212bd081bbbc761d57d33ec9af3cefc7d012
italo manoel <32418036+italomanoel@users.noreply.github.com>
2022-08-18 10:30:02 -0300

    Create TesteCrackingCode.java

---
+++ TesteCrackingCode.java
@@ -0,0 +1,17 @@
+//package e imports
+/**
+ * Um array tipo double simples onde a condição for é o tamanho do array "length", e mostrar quantas posições existentes no console. 
+ * @author italomanoel
+ * 
+ */
+
+public class TesteSobreCrackingCode {
+
+	public static void main(String[] args) {
+		double arranjo[] = new double[5];		
+		for(int i=0; i<arranjo.length; i++){
+			arranjo[i] = i;
+		       System.out.println(arranjo[i]);
+		   }
+	}
+}
