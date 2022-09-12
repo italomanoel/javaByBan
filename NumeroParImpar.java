e5c56293a6fb0283511a1e8fc0d84a60608e4df2
italo manoel <32418036+italomanoel@users.noreply.github.com>
2022-09-12 12:14:52 -0300

    create NumeroParImpar.java

---
+++ NumeroParImpar.java
@@ -0,0 +1,14 @@
+/**
+ * @author italomanoel
+ * Checagem de números ímpar/par em uma sequência.
+ */
+public class NumeroParImpar {
+
+	public static void main(String[] args) {
+		  for(i = 1; i < 16; i++){
+        if(i%2 != 0){
+          System.out.println("É ímpar "+i);
+        }
+      }
+	}
+}
